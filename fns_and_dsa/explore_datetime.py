b5d7c23b27258e636c2b9b7e574eb2617dc6b4c0
GoodnewsAtekha <goodnewsatekha@gmail.com>
2025-06-08 22:31:33 +0100

    Use this to automatically check future date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,30 +1,19 @@
+
 from datetime import datetime, timedelta
 
+# Part 1: Display the current date and time
 def display_current_datetime():
-    # Get the current date and time
-    current_date = datetime.now()
+    current_date = datetime.now()  # Save the current date
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
-    # Format as "YYYY-MM-DD HH:MM:SS"
-    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-
-    print("Current Date and Time:", formatted_date)
-
+# Part 2: Calculate a future date
 def calculate_future_date():
     try:
-        # Ask user how many days to add
-        days = int(input("Enter the number of days to add: "))
-
-        # Get today's date
-        current_date = datetime.now()
-
-        # Add the number of days using timedelta
-        future_date = current_date + timedelta(days=days)
-
-        # Format and print the future date
-        print("Future Date after", days, "days will be:", future_date.strftime("%Y-%m-%d"))
-
+        number_of_days = int(input("Enter the number of days to add to the current date: "))
+        future_date = datetime.now() + timedelta(days=number_of_days)  # Save the future date
+        print("Future date:", future_date.strftime("%Y-%m-%d"))
     except ValueError:
-        print("Please enter a valid integer.")
+        print("Invalid input. Please enter a valid integer.")
 
 def main():
     display_current_datetime()
