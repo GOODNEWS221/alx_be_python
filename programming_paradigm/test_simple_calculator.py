9d5feb88f1477b6566e0cd38793866aa1ec55d36
GoodnewsAtekha <goodnewsatekha@gmail.com>
2025-06-15 20:20:34 +0100

    python -m unittest test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -19,13 +19,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiply method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
-    def test_divide(self):
+    def test_division(self):
         """Test the divide method."""
         self.assertEqual(self.calc.divide(10, 2), 5.0)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
@@ -34,4 +34,3 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test division by zero returns None."""
         self.assertIsNone(self.calc.divide(5, 0))
 
-        
